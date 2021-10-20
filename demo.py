d7f277ef7a219bfd0afc30ce0bd3eb8d1b6063e2
hackhaha1 <hankchui88@gmail.com>
2021-10-20 21:03:55 +0800

    wrong file to dump

--- demo.py
+++ demo.py
@@ -31,6 +31,7 @@ with mp_pose.Pose(
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
     # Flip the image horizontally for a selfie-view display.
+    image = cv2.putText(image, 'Open')
     cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
