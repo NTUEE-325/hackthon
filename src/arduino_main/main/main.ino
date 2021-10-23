65b9859fd86c24419048558bd835513fb0689ddf
hackhaha1 <hankchui88@gmail.com>
2021-10-23 17:46:50 +0800

    1023 17:45 stepper turn bug fixed

--- src/arduino_main/main/main.ino
+++ src/arduino_main/main/main.ino
@@ -1,3 +1,160 @@
+/*#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+
+#include "bluetooth.h"
+#include "lcd.h"
+#include "motor.h"
+
+#define GYMpin A1
+#define NIGHTpin A2
+#define STUDYpin A3
+int xanglechange = 0;
+int yanglechange = 0;
+bool flag1 = true;
+bool flag2 = true;
+int steps1 = 0;
+int steps2 = 0;
+int laststeps1 =0;
+int laststeps2 =0;
+void setup()
+{
+  Serial.begin (9600);             // Serial Port begin
+  setup_BT();
+  setup_lcd();
+  setup_motor();
+  pinMode(GYMpin,OUTPUT);
+  pinMode (NIGHTpin,OUTPUT);
+  pinMode (STUDYpin, OUTPUT);
+  //lcd.setCursor(0,3);
+  //lcd.print("Happy Using!!!");
+  //delay(5000);
+  //lcd.setCursor(0,3);
+  //lcd.print(clear_line);
+}
+
+void loop() {
+  MODE mode = GETMODE();
+  if (mode == DEFAULT_MODE){
+    lcd.setCursor(0,1);
+    //lcd.print(clear_line);
+    //lcd.setCursor(0,2);
+    lcd.print("DEFAULT_MODE        ");
+    lcd.setCursor(0,2);
+    lcd.print(clear_line);
+    //Serial.write("default");
+    analogWrite(GYMpin , 0);
+    analogWrite(NIGHTpin , 0);
+    analogWrite(STUDYpin , 0);
+    mode = NOTHING;
+  }
+  else if (mode == NIGHT_MODE){
+    lcd.setCursor(0,1);
+    //lcd.print(clear_line);
+    //lcd.setCursor(0,1);
+    lcd.print("NIGHT_MODE          ");
+    lcd.setCursor(0,2);
+    lcd.print(clear_line);
+    //Serial.write("night");
+    analogWrite(GYMpin , 0);
+    analogWrite(NIGHTpin , 200);
+    analogWrite(STUDYpin , 0);
+    mode = NOTHING;   
+  }
+  else if (mode == GYM_MODE){
+    lcd.setCursor(0,1);
+    //lcd.print(clear_line);
+    //lcd.setCursor(0,1);
+    lcd.print("GYM_MODE            ");
+    lcd.setCursor(0,2);
+    lcd.print(clear_line);
+    //Serial.write("gym");
+    analogWrite(GYMpin , 200);
+    analogWrite(NIGHTpin , 0);
+    analogWrite(STUDYpin , 0);
+    mode = NOTHING;   
+  }
+  else if (mode == STUDY_MODE){
+    lcd.setCursor(0,1);
+    //lcd.print(clear_line);
+    //lcd.setCursor(0,1);
+    lcd.print("STUDY_MODE          ");
+    lcd.setCursor(0,2);
+    lcd.print(clear_line);
+    //Serial.write("study");
+    analogWrite(GYMpin , 0);
+    analogWrite(NIGHTpin , 0);
+    analogWrite(STUDYpin , 200);
+    mode = NOTHING;
+  }
+  else if (mode == WIND_UPDOWN){
+    lcd.setCursor(0,1);
+    lcd.print(clear_line);
+    lcd.setCursor(0,1);
+    lcd.print("WIND_UPDOWN");
+    xanglechange = countXAngle();
+    lcd.setCursor(0,2);
+    lcd.print("TURN_UD ");
+    lcd.print(xanglechange);
+    laststeps1 = steps1;
+    steps1 = Return_steps1(xanglechange);
+    if (stepper1.currentPosition() != steps1){
+      Motor_UD(steps1 , laststeps1);
+      if (stepper1.currentPosition() != steps1) flag1 = false;
+      else flag1 = true;
+    }
+    mode = NOTHING; 
+  }
+  else if (mode == WIND_RIGHTLEFT){
+    lcd.setCursor(0,1);
+    lcd.print(clear_line);
+    lcd.setCursor(0,1);
+    lcd.print("WIND_RIGHTLEFT");
+    yanglechange = countYAngle();
+    lcd.setCursor(0,2);
+    lcd.print("TURN_RL ");
+    lcd.print(yanglechange);
+    laststeps2 = steps2;
+    steps2 = Return_steps2(yanglechange);
+    if (stepper2.currentPosition() != steps2 ){
+      Motor_UD(steps2 , laststeps2);
+      if (stepper2.currentPosition() != steps2) flag2 = false;
+      else flag2 = true;
+    }
+    mode = NOTHING;
+  }
+  else if (mode == WIND_STRONG){
+    lcd.setCursor(0,2);
+    //lcd.print(clear_line);
+    //lcd.setCursor(0,1);
+    lcd.print("WIND_STRONG         ");
+    //Serial.write("wind_strong");
+    lcd_windstrong();
+    mode = NOTHING;
+  }
+  else if (mode == WIND_WEAK){
+    lcd.setCursor(0,2);
+    //lcd.print(clear_line);
+    //lcd.setCursor(0,1);
+    lcd.print("WIND_WEAK           ");
+    //Serial.write("wind_weak");
+    lcd_windsmall();
+    mode = NOTHING;
+  }
+  else if(mode == NOTHING){
+    lcd.setCursor(0,1);
+    if (flag1 == false){
+      Motor_UD(steps1 , laststeps1);
+      if (stepper1.currentPosition() == steps1) flag1 = true;
+    }
+    if (flag2 == false){
+      Motor_RL(steps2 , laststeps2);
+      if (stepper2.currentPosition() == steps2) flag2 = true;
+    }
+    
+    //lcd.print(clear_line);
+  }
+}*/
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
@@ -15,6 +172,8 @@ bool flag1 = true;
 bool flag2 = true;
 int steps1 = 0;
 int steps2 = 0;
+int laststeps1 =0;
+int laststeps2 =0;
 void setup()
 {
   Serial.begin (9600);             // Serial Port begin
@@ -94,6 +253,9 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print("TURN_UD ");
     lcd.print(xanglechange);
+    //laststeps1 = steps1;
+    Serial.write("steps:\n");
+    Serial.println(steps1);
     steps1 = Return_steps1(xanglechange);
     if (stepper1.currentPosition() != steps1){
       Motor_UD(steps1);
@@ -115,6 +277,10 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print("TURN_RL ");
     lcd.print(yanglechange);
+    //laststeps2 = steps2;
+    Serial.write("steps:\n");
+    Serial.println(steps2);
+    //Serial.println(laststeps2);
     steps2 = Return_steps2(yanglechange);
     if (stepper2.currentPosition() != steps2 ){
       Motor_UD(steps2);
@@ -167,59 +333,4 @@ void loop() {
     //lcd.print(clear_line);
   }
 }
-  /*  
-  switch(mode){
-    case DEFAULT_MODE:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("DEFAULT_MODE");
-      digitalWrite(GYMpin , LOW);
-      digitalWrite(NIGHTpin , LOW);
-      digitalWrite(STUDYpin , LOW);
-    case NIGHT_MODE:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("NIGHT_MODE");
-      digitalWrite(GYMpin , LOW);
-      digitalWrite(NIGHTpin , HIGH);
-      digitalWrite(STUDYpin , LOW);
-    case GYM_MODE:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("GYM_MODE");
-      digitalWrite(GYMpin , HIGH);
-      digitalWrite(NIGHTpin , LOW);
-      digitalWrite(STUDYpin , LOW);
-    case STUDY_MODE:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("STUDY_MODE");
-      digitalWrite(GYMpin , LOW);
-      digitalWrite(NIGHTpin , LOW);
-      digitalWrite(STUDYpin , HIGH);
-    case WIND_UPDOWN:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("WIND_UPDOWN");
-      anglechange = countXAngle();
-      Motor_UD(anglechange); 
-    case WIND_RIGHTLEFT:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("WIND_RIGHTLEFT");
-      anglechange = countYAngle();
-      MOTOR_RL(anglechange);
-    case WIND_STRONG:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("WIND_STRONG");
-      lcd_windstrong();
-    case WIND_WEAK:
-      lcd.setCursor(0,0);
-      lcd.print(clear_line);
-      lcd.print("WIND_WEAK");
-      lcd_windsmall();
-    default:
-      break;
-  }
-  */
+ 
--- src/arduino_main/main/motor.h
+++ src/arduino_main/main/motor.h
@@ -1,8 +1,6 @@
 #ifndef MOTOR.H
 #define MOTOR.H
 
-#include <AccelStepper.h>
-
 #define motor1Pin1  4     // IN1 on the ULN2003 driver
 #define motor1Pin2  5      // IN2 on the ULN2003 driver
 #define motor1Pin3  6     // IN3 on the ULN2003 driver
@@ -12,7 +10,13 @@
 #define motor2Pin3  10     // IN3 on the ULN2003 driver
 #define motor2Pin4  11     // IN4 on the ULN2003 driver
 
+
+
+#include <AccelStepper.h>
+
+
 #define MotorInterfaceType 8
+
 int motorspeed = 500;
 
 AccelStepper stepper1 = AccelStepper(MotorInterfaceType, motor1Pin1, motor1Pin3, motor1Pin2, motor1Pin4);
@@ -26,27 +30,22 @@ void setup_motor(){
   stepper2.setCurrentPosition(0);
 }
 int Return_steps1(int x){
-  return x*50; //計算葉片上揚x度時，齒輪需要轉幾個step (4096 step =一圈)
+  return x*150 ; //計算葉片上揚x度時，齒輪需要轉幾個step (4096 step =一圈)
 }
-void Motor_UD(int x){
-  if (x<=0) stepper1.setSpeed(-1*motorspeed);
+void Motor_UD(int steps ){
+  if ((steps-stepper1.currentPosition())<=0) stepper1.setSpeed(-1*motorspeed);
   else stepper1.setSpeed(motorspeed);
   stepper1.runSpeed();
-  /*
-  while (stepper1.currentPosition() != steps) {
-    if (x<=0) stepper1.setSpeed(-1*motorspeed);
-    else stepper1.setSpeed(motorspeed);
-    stepper1.runSpeed();
-  }
-  */
+  
 }
 int Return_steps2(int x){
-  return x*50; //計算葉片上揚x度時，齒輪需要轉幾個step (4096 step =一圈)
+  return x*150; //計算葉片上揚x度時，齒輪需要轉幾個step (4096 step =一圈)
 }
-void Motor_RL(int x){
-  if (x<=0) stepper2.setSpeed(-1*motorspeed);
+void Motor_RL(int steps ){
+  if ((steps-stepper2.currentPosition())<=0) stepper2.setSpeed(-1*motorspeed);
   else stepper2.setSpeed(motorspeed);
   stepper2.runSpeed();
+
 }
 
 #endif
